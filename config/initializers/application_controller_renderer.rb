320efba58e807a42627c113611553e01c32bdb48
kazuya120 <songlinheye2@gmail.com>
2021-11-09 06:32:13 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0c8ba608d1a6b0827da7e206c21aeea711ededee05d04c1ebed96d28d578ab846eed2fc7c81e29e3703e947254c7bf4ea842f860bdd1b416b5b5716aa15f8608'
---
+++ config/routes.rb
@@ -0,0 +1,59 @@
+Rails.application.routes.draw do
+   # admin
+  devise_for :admin, :controllers => {
+    :sessions => 'admin/sessions',
+    :registrations => 'admin/registrations',
+  }
+  namespace :admin do
+    resources :customers,only:[:index, :show, :edit, :update]
+     resources :items, only:[:index, :new, :create, :show, :edit, :update]
+     resources :genres, only:[:index, :create, :edit, :update, :show]
+     resources :orders, only:[:index, :show, :update] do
+       member do
+         get :current_index
+         resource :order_details,only:[:update]
+       end
+       collection do
+         get :today_order_index
+       end
+     end
+   end
+
+  # customer
+  devise_for :customers, :controllers => {
+    :sessions => 'public/sessions',
+    :registrations => 'public/registrations',
+    :passwords => 'public/passwords'
+  }
+  get 'about' => 'public/homes#about'
+  root 'public/homes#top'
+  get '/customers/contact' => 'public/customers#contact'
+
+  scope module: :public do
+    resources :items,only:[:index, :show]
+    get 'edit/customers' => 'customers#edit'
+    patch 'update/customers' => 'customers#update'
+
+    resource :customers,only:[:edit, :update, :show] do
+      collection do
+        get 'quit'
+        patch 'out'
+      end
+    end
+
+    resources :cart_items,only:[:index, :update, :create, :destroy] do
+      collection do
+        delete '/' => 'cart_items#all_destroy'
+      end
+    end
+
+    resources :orders,only:[:new, :index, :show, :create] do
+      collection do
+        post 'confirm'
+        get 'thanx'
+      end
+    end
+
+    resources :addresses,only:[:index, :create, :edit, :update, :destroy]
+  end
+end
